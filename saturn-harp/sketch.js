8bdb3f0e20a7cc7694d6530b9197b9290dadfeba
matt-russo <russomatt00@gmail.com>
2018-04-23 23:24:15 -0400

    Added comments

--- saturn-harp/sketch.js
+++ saturn-harp/sketch.js
@@ -1,36 +1,37 @@
-var imWidth = 1959, imHeight = 1000;
-var cnv;
-var imScale;
+var imWidth = 1959, imHeight = 1000; // hard coded image dimensions
+var cnv; // canvas
+var imScale; // used to adjust canvas size based on window size
 
-var nNotes = 15;
-var minValue = 30;
-var step = (256. - minValue) / nNotes;
+var nNotes = 15; // number of note steps   */
+var minValue = 30; // minimum pixel value to count as a note
+var step = (256. - minValue) / nNotes; // pixel value step size
 var pixNum = 0, pixNum0 = 0, lastPixNum = 0, lastPixNum0 = 0;
 
 var mode, harmony;
 var notesMajor = [];
 var notesMinor = [];
 
-var autox0 = 0, autoy0 = 0, autox = 0, autoy = 0;
+var autox0 = 0, autoy0 = 0, autox = 0, autoy = 0; // initialize mouse positions */
 
 var speed = .1;
-var maxSpeed = 40.;
-var minSpeed = 0.01;
-var speedSign = 1;
+var maxSpeed = 26; // doublings of 0.1 gets to 25.6
+var minSpeed = 0.01; // halvings of 0.1 gets to .0125
+var speedSign = 1;   // initial direction (positive is downwards)
 
 var cassini, cassiniAuto;
 
 var touchIsDown = false;
 
-
+// preload background and cursor images
 function preload() {
   img = loadImage("./images/BRing.jpg");
   img2 = loadImage("./images/BRing2.jpg");
   cassiniImg = loadImage("./images/cassini.png");
   soundFormats('mp3');
+
 }
 
-
+// set up canvas, background image, and cursors
 function setup() {
   cnv = createCanvas(imWidth, imHeight);
   cnv.parent('canvas');
@@ -43,7 +44,7 @@ function setup() {
   cassini = new cursorIm(0, 0);
   cassiniAuto = new autoCursor(0, 0);
 
-  init();
+  init(); // load all sound files and set initial mode and harmony
 }
 
 
@@ -65,16 +66,17 @@ function draw() {
   if (mode == 'manual') {
     if (mouseIsPressed || touchIsDown) {
 
+      // find current pixel number and pixel value
       pixNum = 4 * (Math.round(mouseX * imScale) + Math.round(mouseY * imScale) * imWidth); //labels pixel
       pixValue = (img.pixels[pixNum] + img.pixels[pixNum + 1] + img.pixels[pixNum + 2]) / 3.;
       pixValue = Math.pow(pixValue / 256, 1.5) * 256; //scale brightness
 
       if (pixNum != lastPixNum) {
-        playNotes();
+        playNotes(); // trigger note for this pixel value
       }
       lastPixNum = pixNum;
 
-      cassini.update(mouseX, mouseY);
+      cassini.update(mouseX, mouseY); // update cursor postion
       cassini.show();
     }
   }
@@ -83,19 +85,20 @@ function draw() {
 
     autox += speedSign * speed;
     autoy = autoy0 + imHeight / imWidth * (autox - autox0);
-    checkBounce();
+    checkBounce(); // check for bouncing off walls
 
+    // find current pixel number and pixel value
     pixNum0 = Math.round(autox * imScale) + Math.round(autoy * imScale) * imWidth;
     pixNum = Math.round(4 * pixNum0); //labels pixel
     pixValue = (img.pixels[pixNum] + img.pixels[pixNum + 1] + img.pixels[pixNum + 2]) / 3.;
     pixValue = Math.pow(pixValue / 256, 1.5) * 256; //scale brightness
 
     if (pixNum0 != lastPixNum0) {
-      playNotes();
+      playNotes(); // trigger note for this pixel value
     }
     lastPixNum0 = pixNum0;
 
-    cassiniAuto.update(autox, autoy);
+    cassiniAuto.update(autox, autoy); // update cursor
     cassiniAuto.show();
   }
 }
@@ -117,7 +120,6 @@ function init() {
 
 function touchStarted() {
   if (mode == "automatic") {
-
     if (mouseX >= 0 && mouseX <= imWidth / imScale && mouseY >= 0 && mouseY <= imHeight / imScale) {
       autox = mouseX;
       autoy = mouseY;
@@ -219,6 +221,8 @@ function makeMajor() {
   document.getElementById('minor_button').classList.add("hidden");
 }
 
+// cursor object for automatic mode
+
 function autoCursor(x, y) {
   this.x = x;
   this.y = y;
@@ -233,6 +237,8 @@ function autoCursor(x, y) {
 
   }
 }
+
+// cursor object for manual mode
 
 function cursorIm(x, y) {
   this.x = x;
